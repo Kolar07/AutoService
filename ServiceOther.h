9c63efb716b776b398b591907024896b0c5fc524
Jakub Kolarczyk <jakubkolarczyk@gmail.com>
2024-05-24 23:55:20 +0200

    Polymorphism Service implemented

--- ServiceOther.h
+++ ServiceOther.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef SERVICE_OTHER_H_
+#define SERVICE_OTHER_H_
+
+#include <iostream>
+#include <vector>
+#include <ostream>
+#include "Service.h"
+
+class ServiceOther : public Service {
+
+private:
+	std::string type = "Other";
+	std::string other;
+
+public:
+	ServiceOther(int mileage_, std::string _other);
+	//std::ostream& operator<<(const std::ostream& os);
+
+	virtual void print() override;
+
+	std::string getType();
+	std::string getOther();
+
+};
+
+
+#endif
--- ServiceTiming.h
+++ ServiceTiming.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef SERVICE_TIMING_H_
+#define SERVICE_TIMING_H_
+
+#include <iostream>
+#include <vector>
+#include <ostream>
+#include "Service.h"
+
+class ServiceTiming : public Service {
+
+private:
+	std::string type = "Timing";
+	std::string timing;
+
+public:
+	ServiceTiming(int mileage_, std::string _timing);
+	//std::ostream& operator<<(const std::ostream& os);
+
+	virtual void print() override;
+
+	std::string getType();
+	std::string getTiming();
+
+};
+
+
+#endif
